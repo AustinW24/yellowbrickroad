aa0d78545300671a9dbc69d1b2fbbaf74c7090a3
Austin Williams <austinwill264@gmail.com>
2021-12-02 15:08:46 -0800

    longest substr

--- DSA.js
+++ DSA.js
@@ -1,18 +1,45 @@
-function isBalanced(someString){
-    if(someString[0] != "(") return false;
-    if(!someString.length) return true;
-    let stack = [];
-    for(let i = 0; i < someString.length; i++){
-        if(someString[i] == "("){
-            stack.push(someString[i])
-        } else if(someString[i] == ")"){
-            stack.pop()
-        }
-    }
-    if(!stack.length) {
-        return true;
-    } else {
+// function isBalanced(someString){
+//     if(someString[0] != "(") return false;
+//     if(!someString.length) return true;
+//     let stack = [];
+//     for(let i = 0; i < someString.length; i++){
+//         if(someString[i] == "("){
+//             stack.push(someString[i])
+//         } else if(someString[i] == ")"){
+//             stack.pop()
+//         }
+//     }
+//     if(!stack.length) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+const isUnique = (str) => {
+let set = new Set();
+for(let i = 0; i < str.length; i++){
+    if(set.has(str[i])){
         return false;
+    } else {
+        set.add(str[i])
     }
 }
-console.log(isBalanced("(a(()b))"))
+return true;
+}
+const longestSubString = (string) => {
+    //create a maxCounter && currentMaxCounter
+    let max = 0;
+
+    for(let i = 0; i < string.length; i++){
+        for(let k = i+1; k < string.length;k++){
+            let subString = string.substring(i, k)
+            if(isUnique(subString)){
+                max = Math.max(max, subString.length)
+            }
+        }
+    }
+    return max;
+}
+
+
+console.log(longestSubString("bbabcbcadbb"))
